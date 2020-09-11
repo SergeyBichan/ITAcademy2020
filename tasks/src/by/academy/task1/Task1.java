1a18cbc3a4ae2145f5fae080dc557b85976c541a
Sergey <ugidancer@Gmail.com>
2020-09-11 22:04:50 +0300

    trying to do hometask v1.0

---
+++ tasks/src/by/academy/task1/Task1.java
@@ -0,0 +1,72 @@
+package by.academy.task1;
+
+
+public class Task1 {
+    static byte AG;
+    static short BG;
+    static int CG;
+    static long DG;
+    static float EG;
+
+    public static byte getAG() {
+        return AG;
+    }
+
+    public static void setAG(byte AG) {
+        Task1.AG = AG;
+    }
+
+    public static short getBG() {
+        return BG;
+    }
+
+    public static void setBG(short BG) {
+        Task1.BG = BG;
+    }
+
+    public static int getCG() {
+        return CG;
+    }
+
+    public static void setCG(int CG) {
+        Task1.CG = CG;
+    }
+
+    public static long getDG() {
+        return DG;
+    }
+
+    public static void setDG(long DG) {
+        Task1.DG = DG;
+    }
+
+    public static float getEG() {
+        return EG;
+    }
+
+    public static void setEG(float EG) {
+        Task1.EG = EG;
+    }
+
+    public static void main(String[] args) {
+        byte al;
+        short bl;
+        int cl;
+        long dl;
+        float el;
+        double fl;
+        char gl;
+        String s;
+
+        Byte bte;
+        Integer integr;
+        Short shrt;
+        Long lng;
+        Float flt;
+        Double dbl;
+        Character chr;
+        Boolean bln;
+
+
+    }
+}
