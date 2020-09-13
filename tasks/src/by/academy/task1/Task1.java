c82b1d6f288ebbe64e8872cd963046c15ffd53e6
Sergey Bichan <ugidancer@gmail.com>
2020-09-13 14:36:06 +0300

    Осознание начало приходить..

--- tasks/src/by/academy/task1/Task1.java
+++ tasks/src/by/academy/task1/Task1.java
@@ -8,7 +8,15 @@ public class Task1 {
     static long dg;
     static float eg;
     static double fg;
+    static char chg;
 
+    public static char getChg() {
+        return chg;
+    }
+
+    public static void setChg(char chg) {
+        Task1.chg = chg;
+    }
 
     public static byte getAg() {
         return ag;
@@ -77,12 +85,15 @@ public class Task1 {
         Boolean bln;
 
 
-        Task1.setAg((byte) 99);
-        Task1.setBg((short) 1300);
-        Task1.setCg((int) 24599222);
-        Task1.setDg((long) 956541846);
+        Task1.setAg((byte) 98);
+        Task1.setBg((short) 1154);
+        Task1.setCg(549845);
+        Task1.setDg(46843215);
         Task1.setEg((float) 3.1255);
-        Task1.setFg((double) 3.116848);
+        Task1.setFg(3.116848);
+        Task1.setChg(('b'));
+
+
         System.out.println("Global variables are: ");
         System.out.println();
         System.out.println("Variable ag is: " + Task1.getAg());
@@ -91,6 +102,7 @@ public class Task1 {
         System.out.println("Variable dg is: " + Task1.getDg());
         System.out.println("Variable eg is: " + Task1.getEg());
         System.out.println("Variable fg is: " + Task1.getFg());
+        System.out.println("Variable chg is: " + Task1.getChg());
         System.out.println();
 
 
@@ -100,6 +112,7 @@ public class Task1 {
         dl = Task1.getDg();
         el = Task1.getEg();
         fl = Task1.getFg();
+        gl = Task1.getChg();
 
 
         System.out.println("Locale variables are: ");
@@ -110,7 +123,35 @@ public class Task1 {
         System.out.println("Variable dl is: " + dl);
         System.out.println("Variable el is: " + el);
         System.out.println("Variable fl is: " + fl);
+        System.out.println("Variable gl is: " + gl);
         System.out.println();
+
+
+
+        Task1.setAg((byte)bg);
+        Task1.setChg((char)bg);
+        Task1.setDg(cg);
+        System.out.println("dg is: "+ dg);
+        System.out.println("char is: " + chg);
+
+
+        integr = Task1.getCg();
+        bte = Task1.getAg();
+        shrt = Task1.getBg();
+        lng = Task1.getDg();
+        flt = Task1.getEg();
+        dbl = Task1.getFg();
+        chr = Task1.getChg();
+
+
+        System.out.println("integer is: "+ integr);
+        System.out.println("bte is: " + bte);
+        System.out.println("shrt is: " + shrt);
+        System.out.println("lng is: " + lng);
+        System.out.println("flt is: " + flt);
+        System.out.println("dbl is: " + dbl);
+        System.out.println("chr is: " + chr);
+
 
 
 
