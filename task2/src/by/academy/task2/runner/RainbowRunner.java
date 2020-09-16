f597419bc0f23e691dfac39622fc7f6d45ea997f
Sergey <ugidancer@Gmail.com>
2020-09-16 16:47:34 +0300

    Task2

--- task2/src/by/academy/task2/runner/RainbowRunner.java
+++ task2/src/by/academy/task2/runner/RainbowRunner.java
@@ -7,8 +7,8 @@ public class RainbowRunner {
     public static void main(String[] args) {
 
         Rainbow rainbow = new Rainbow();
-        rainbow.chooseColour((byte) 0);
-        rainbow.chooseColourSecond((byte) 2, (byte) 6);
+        rainbow.chooseColour((byte) 6);
+        rainbow.chooseColourSecond((byte) 2, (byte) 4);
 
 
     }
