269ab8e1609792d350a42d65f86edf4a9bcd5302
Sergey Bichan <ugidancer@gmail.com>
2020-09-19 16:15:17 +0300

    Trying cycles for cats

--- task3/src/by/academy/cats/Cat.java
+++ task3/src/by/academy/cats/Cat.java
@@ -22,14 +22,6 @@ public class Cat {
         this.age = age;
     }
 
-    public int getSumCat() {
-        return sumCat;
-    }
-
-    public void setSumCat(int sumCat) {
-        this.sumCat = sumCat;
-    }
-
 
     public Cat() {
     }
@@ -42,38 +34,27 @@ public class Cat {
             int i = (int) (Math.random() * str.length());
             char l = str.charAt(i);
             result = result + l;
-
         }
         return result;
     }
 
     public static int getRandomAge() {
         int result = 0;
-
         int i = (int) (Math.random() * 20);
         result = result + i;
         return result;
-
-
     }
-
 
     public void CreatingCatsByWhile() {
         while (sumCat < 10) {
-
             System.out.println("Name: " + getName() + " " + "Age: " + getAge());
             sumCat++;
         }
     }
 
-
     public void CreatingCatsByFor() {
         for (int i = 0; i < 10; i++) {
-
             System.out.println(getRandomAge() + "," + getRandomName());
-
         }
     }
-
-
 }
--- task3/src/by/academy/runner/CatRunner.java
+++ task3/src/by/academy/runner/CatRunner.java
@@ -6,15 +6,12 @@ import by.academy.cats.Cat;
 public class CatRunner {
     public static void main(String[] args) {
         Cat cat = new Cat();
-        //cat.CreatingCatsByWhile();
+        cat.CreatingCatsByWhile();
 
-        cat.setName(Cat.getRandomName());
+        System.out.println(cat.getRandomName());
+        System.out.println(cat.getRandomAge());
 
-//        System.out.println(cat.getRandomName());
-//        System.out.println(cat.getRandomAge());
-
-       // cat.CreatingCatsByFor();
-
+        cat.CreatingCatsByFor();
 
 
     }
