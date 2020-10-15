ec637a88d3ad57dbfa6bcc9e5b2e96a66737a9c9
ugidancer@Gmail.com <SansampgtsA457445>
2020-10-15 21:18:36 +0300

    task9_ЗАЕБИСЬ

---
+++ lesson_10/src/Main.java
@@ -0,0 +1,43 @@
+import com.sun.xml.internal.ws.api.model.wsdl.WSDLOutput;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        while (true){
+            try {
+                Scanner sc = new Scanner(System.in);
+                System.out.println("Магазин");
+                System.out.println("Первый");
+                System.out.println("Второй");
+                System.out.println("Третий для выхода");
+                int num = sc.nextInt();
+                if (1 == num) {
+                    workingWithGoods();
+                }
+                else if (2 == num){
+                    workingWithOrders();
+                }
+                else if (3 == num){
+                    break;
+                }
+                else {
+                    System.out.println("Введите 1 или 2");
+                }
+
+            } catch (Exception e) {
+                System.out.println("Введите корректный символ");;
+            }
+        }
+        }
+
+
+
+    private static void workingWithGoods(){
+
+    }
+    private static void workingWithOrders(){
+
+    }
+
+}
